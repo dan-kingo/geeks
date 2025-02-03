f9798c229ce959cef424c873da8cd0ce77e7344f
Dan Kingo <danieldejen23@gmail.com>
2025-02-03 23:08:15 +0300

    add profile picture

--- src/social-profile/SocialProfile.tsx
+++ src/social-profile/SocialProfile.tsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import "./social.css";
+import image from "../assets/image.png";
 const SocialProfile = () => {
   const socialLinks = [
     "Github",
@@ -11,8 +12,13 @@ const SocialProfile = () => {
   ];
   return (
     <div className="social-card">
-      {/* <Button color="custom"> Custom</Button> */}
-
+      <img className="rounded  mb-3" src={image} alt="image" />
+      <h5 style={{ color: "#fff" }} className="mb-2">
+        Dan Kingo
+      </h5>
+      <h6 style={{ color: "orange" }} className="mb-3">
+        Bahir Dar, Ethiopia
+      </h6>
       {socialLinks.map((link, index) => (
         <Button color="custom" key={index}>
           {link}
